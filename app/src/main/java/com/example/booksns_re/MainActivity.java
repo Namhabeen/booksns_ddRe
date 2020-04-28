9b74ab82857d7456456ba7c0334f3862884c78f2
hwon22 <s2018s06@e-mirim.hs.kr>
2020-04-28 09:04:50 +0900

    hw_part 1/2/3/4 push test

--- app/src/main/java/com/example/booksns_re/MainActivity.java
+++ app/src/main/java/com/example/booksns_re/MainActivity.java
@@ -1,14 +1,93 @@
 package com.example.booksns_re;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        findViewById(R.id.loginButton).setOnClickListener(onClickListener);
+        findViewById(R.id.forgotGo).setOnClickListener(onClickListener);
+        findViewById(R.id.signUpGo).setOnClickListener(onClickListener);
+    }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.loginButton:
+                    login();
+                    break;
+
+                case R.id.forgotGo:
+                    showToast(MainActivity.this, "비밀번호 찾기 창으로 이동합니다. (개발중)");
+                    myStartActivity(SignUpTwoActivity.class);
+                    break;
+
+                case R.id.signUpGo:
+                    showToast(MainActivity.this, "회원가입 창으로 이동합니다!");
+                    myStartActivity(SignUpOneActivity.class);
+                    break;
+
+            }
+        }
+    };
+
+    private void login() {
+        String email = ((EditText) findViewById(R.id.idInput)).getText().toString();
+
+        String password = ((EditText) findViewById(R.id.pwInput)).getText().toString();
+
+        if (email.length() > 0 && password.length() > 0) {
+
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                showToast(MainActivity.this, "로그인에 성공하였습니다.");
+                                myStartActivity(SignUpThreeActivity.class);
+                            } else {
+                                if (task.getException() != null) {
+                                    showToast(MainActivity.this, "아이디 또는 비밀번호를 확인해주세요");
+                                    showToast(MainActivity.this, task.getException().toString());
+                                }
+                            }
+                        }
+                    });
+        } else {
+            showToast(MainActivity.this, "아이디 또는 비밀번호를 입력해 주세요.");
+        }
+    }
+
+    private void myStartActivity(Class c) {
+        Intent intent = new Intent(this, c);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+    public static void showToast(Activity activity, String msg) {
+        Toast.makeText(activity, msg, Toast.LENGTH_SHORT).show();
+        //Toast.makeText(activity, msg, Toast.LENGTH_LONG).show();
     }
 }
